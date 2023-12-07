8abed96698c2812b8f4ee1760b6594ae8a566da2
lunatax85 <abenjamindev85@gmail.com>
2023-12-06 23:28:12 -0300

    Edit CRUD

--- DESARROLLO/userservice/src/main/java/com/data/userservice/controller/UserController.java
+++ DESARROLLO/userservice/src/main/java/com/data/userservice/controller/UserController.java
@@ -1,5 +1,23 @@
 package com.data.userservice.controller;
 
+import com.data.userservice.model.User;
+import com.data.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/api/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+
 
 }
