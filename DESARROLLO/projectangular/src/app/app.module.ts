d9a647fdc601f6893ae9ef8fa45c8925b11f0243
andresbluna <andres.benjamin.luna@gmail.com>
2024-04-21 11:21:11 -0400

    update header

--- DESARROLLO/projectangular/src/app/app.module.ts
+++ DESARROLLO/projectangular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { SliderComponent } from './slider/slider.component';
+import { Header2Component } from './header-2/header-2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    SliderComponent
+    SliderComponent,
+    Header2Component
   ],
   imports: [
     BrowserModule
